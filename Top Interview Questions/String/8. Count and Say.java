a9e88800b908eb950dd1d1d7cd104d8d86a8f3f9
Emily <76611914+yenxuan1381@users.noreply.github.com>
2021-03-26 23:17:04 +0800

    Create 8. Count and Say.java

---
+++ Top Interview Questions/String/8. Count and Say.java
@@ -0,0 +1,81 @@
+/*
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+    countAndSay(1) = "1"
+    countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+
+To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a contiguous section all of the same character. Then for each group, say the number of characters, then say the character. To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
+
+For example, the saying and conversion for digit string "3322251":
+
+Given a positive integer n, return the nth term of the count-and-say sequence.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: "1"
+Explanation: This is the base case.
+
+Example 2:
+
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = say "1" = one 1 = "11"
+countAndSay(3) = say "11" = two 1's = "21"
+countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+
+ 
+
+Constraints:
+
+    1 <= n <= 30
+
+
+*/
+
+// Runtime: 1 ms
+// Memory Usage: 36.3 MB
+
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1){
+            return "1";
+        }
+        
+        String ans = "1";
+        
+        for(int i = 2; i <= n; i++){
+            ans = f(ans);
+        }
+        
+        return ans;
+        
+    }
+    
+    String f(String s){
+        char ch = s.charAt(0);
+        int count = 1;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int j = 1; j < s.length(); j++){
+            
+            if(s.charAt(j) != ch){
+                sb.append(count).append(ch);
+                count = 1;
+                ch = s.charAt(j);
+            }
+            else{
+                count++;
+            }
+        }
+        
+        sb.append(count).append(ch);
+        return sb.toString();
+        
+    }
+}
